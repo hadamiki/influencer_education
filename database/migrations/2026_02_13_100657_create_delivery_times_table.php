934760ec2bfd16c93ed808cb82331b991868401f
miyake <aot_miyake@icloud.com>
2026-02-13 19:37:43 +0900

    Add migrations for curriculums and delivery_times

---
+++ database/migrations/2026_02_13_100657_create_delivery_times_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('delivery_times', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('curriculums_id');
+
+            $table->dateTime('delivery_from')->nullable();
+            $table->dateTime('delivery_to')->nullable();
+            $table->timestamps();
+
+
+            // 外部キー
+            $table->foreign('curriculums_id')->references('id')->on('curriculums');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delivery_times');
+    }
+};
